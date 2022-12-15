0fb2007cd983378d694c9eb1f9d58469ef60849d
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2022-12-15 14:58:34 +0900

    Create solution.py

---
+++ 코딩테스트 연습/코딩테스트 입문/분수의 덧셈/solution.py
@@ -0,0 +1,9 @@
+import math
+
+def solution(denum1, num1, denum2, num2):
+    denum = denum1 * num2 + denum2 * num1
+    num = num1 * num2
+    d = math.gcd(denum, num)
+    
+    answer = [denum // d, num // d]
+    return answer

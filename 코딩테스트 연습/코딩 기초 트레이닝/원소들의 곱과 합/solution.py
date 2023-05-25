765f6aad9a20a8e44c7c52291c66dd8b70bc6258
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-05-25 10:20:46 +0900

    -
    2023-05-25

--- 코딩테스트 연습/코딩 기초 트레이닝/원소들의 곱과 합/solution.py
+++ 코딩테스트 연습/코딩 기초 트레이닝/원소들의 곱과 합/solution.py
@@ -1,1 +1,8 @@
+def solution(num_list):
+    check1, check2 = 1, 0
+    for i in num_list:
+        check1 *= i
+        check2 += i
 
+    answer = 1 if check1 < check2 ** 2 else 0
+    return answer
